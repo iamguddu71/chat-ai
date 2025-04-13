d033d4c4689b16a6a31df18f676d255afc7d6e40
Guddu <iamguddu71@gmail.com>
2025-04-14 02:05:09 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,33 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [question, setQuestion] = useState(0);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
   async function generateAnswer() {
-    console.log("loading..");
+    setAnswer("loading..");
     const response = await axios({
       url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyD1CIg_i77G7UhPBgH9VEXNFNk1OZ7l1N0",
       method: "post",
       data: {
-        contents: [
-          { parts: [{ text: "Write a story about a magic backpack" }] },
-        ],
+        contents: [{ parts: [{ text: question }] }],
       },
     });
-    console.log(
-      response["data"]["candidates"][0]["content"]["parts"][0]["text"]
-    );
+
+    setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
   }
   return (
     <>
       <h1>Chat AI</h1>
-      <textarea value={12} cols="30" rows="10"></textarea>
+      <textarea
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        cols="30"
+        rows="10"
+      ></textarea>
       <button onClick={generateAnswer}>Generate answer</button>
+
+      <pre>{answer}</pre>
     </>
   );
 }
